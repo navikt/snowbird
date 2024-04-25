9eee0212296178ed0c5af05059cd86ea6d804309
Patrick Pedersen <patrick.pedersen@nav.no>
2024-04-25 12:18:10 +0200

    chore: switch build system to setuptool

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="snowbird",
+    version="0.1.0",
+    description=("Snowbird helps configure Snowflake resources for dataproducts"),
+    # package_dir={"": "inbound"},
+    packages=find_packages(include=("snowbird/*,")),
+    author="NAV IT Virksomhetsdatalaget",
+    author_email="virksomhetsdatalaget@nav.no",
+    url="https://github.com/navikt/snowbird",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "permifrost",
+        "pydantic",
+        "pyyaml",
+        "click",
+        "snowflake-sqlalchemy",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "black",
+            "isort",
+        ],
+    },
+    python_requires=">=3.10",
+    entry_points="""
+        [console_scripts]
+        snowbird=snowbird.command:cli
+    """,
+)
