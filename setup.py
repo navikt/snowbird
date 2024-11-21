0f83f78f346ae63057ba9f4ab2fc4357dd7f2fc3
Patrick Pedersen <patrick.pedersen@nav.no>
2024-11-21 16:00:58 +0100

    Bump versjon til v0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="snowbird",
-    version="0.1.1",
+    version="0.1.2",
     description=("Snowbird helps configure Snowflake resources for dataproducts"),
     # package_dir={"": "inbound"},
     packages=find_packages(include=("snowbird/*,")),
