d2d3bcee0287e0dbafcc4ea6418d9fe7974ee8cb
Patrick Pedersen <patrick.pedersen@nav.no>
2025-03-18 16:06:12 +0100

    chore: add tests for plan overview

--- tests/test_plan_overview.py
+++ tests/test_plan_overview.py
@@ -78,3 +78,220 @@ def test_create_transient_schema_overview():
     print(plan_overview)
     result = plan_overview.get("create_schemas")
     assert result == expected
+
+
+def test_create_schema_overview_when_alter_exists():
+    plan = [
+        "create schema if not exists foo",
+        "alter schema foo set comment='bar'",
+    ]
+    expected = ["foo"]
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("create_schemas")
+    assert result == expected
+
+
+def test_modify_schema_overview():
+    plan = [
+        "alter schema foo set comment='bar'",
+    ]
+    expected = ["foo"]
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("modify_schemas")
+    assert result == expected
+
+
+def test_no_modify_schema_overview_when_create_exists():
+    plan = [
+        "create schema if not exists foo",
+        "alter schema foo set comment='bar'",
+    ]
+    expected = []
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("modify_schemas")
+    assert result == expected
+
+
+def test_create_role_overview():
+    plan = [
+        "create role if not exists foo",
+    ]
+    expected = ["foo"]
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("create_roles")
+    assert result == expected
+
+
+def test_create_role_overview_when_alter_exists():
+    plan = [
+        "create role if not exists foo",
+        "alter role foo set comment='bar'",
+    ]
+    expected = ["foo"]
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("create_roles")
+    assert result == expected
+
+
+def test_modify_role_overview():
+    plan = [
+        "alter role foo set comment='bar'",
+    ]
+    expected = ["foo"]
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("modify_roles")
+    assert result == expected
+
+
+def test_no_modify_role_overview_when_create_exists():
+    plan = [
+        "create role if not exists foo",
+        "alter role foo set comment='bar'",
+    ]
+    expected = []
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("modify_roles")
+    assert result == expected
+
+
+def test_create_user_overview():
+    plan = [
+        "create user if not exists foo",
+    ]
+    expected = ["foo"]
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("create_users")
+    assert result == expected
+
+
+def test_create_user_overview_when_alter_exists():
+    plan = [
+        "create user if not exists foo",
+        "alter user foo set comment='bar'",
+    ]
+    expected = ["foo"]
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("create_users")
+    assert result == expected
+
+
+def test_modify_user_overview():
+    plan = [
+        "alter user foo set comment='bar'",
+    ]
+    expected = ["foo"]
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("modify_users")
+    assert result == expected
+
+
+def test_no_modify_user_overview_when_create_exists():
+    plan = [
+        "create user if not exists foo",
+        "alter user foo set comment='bar'",
+    ]
+    expected = []
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("modify_users")
+    assert result == expected
+
+
+def test_create_warehouse_overview():
+    plan = [
+        "create warehouse if not exists foo",
+    ]
+    expected = ["foo"]
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("create_warehouses")
+    assert result == expected
+
+
+def test_create_warehouse_overview_when_alter_exists():
+    plan = [
+        "create warehouse if not exists foo",
+        "alter warehouse foo set comment='bar'",
+    ]
+    expected = ["foo"]
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("create_warehouses")
+    assert result == expected
+
+
+def test_modify_warehouse_overview():
+    plan = [
+        "alter warehouse foo set comment='bar'",
+    ]
+    expected = ["foo"]
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("modify_warehouses")
+    assert result == expected
+
+
+def test_no_modify_warehouse_overview_when_create_exists():
+    plan = [
+        "create warehouse if not exists foo",
+        "alter warehouse foo set comment='bar'",
+    ]
+    expected = []
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("modify_warehouses")
+    assert result == expected
+
+
+def test_grant_select_overview():
+    plan = [
+        "grant select on all tables in schema foo to role bar",
+    ]
+    expected = plan
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("grant_selects")
+    assert result == expected
+
+
+def test_grant_create_overview():
+    plan = [
+        "grant create table on schema foo to role bar",
+    ]
+    expected = plan
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("grant_create")
+    assert result == expected
+
+
+def test_grant_roles_overview():
+    plan = [
+        "grant role bar to role foo",
+    ]
+    expected = plan
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("grant_roles")
+    assert result == expected
+
+
+def test_grant_users_overview():
+    plan = [
+        "grant role bar to user foo",
+    ]
+    expected = plan
+    plan_overview = overview(execution_plan=plan)
+    print(plan_overview)
+    result = plan_overview.get("grant_users")
+    assert result == expected
