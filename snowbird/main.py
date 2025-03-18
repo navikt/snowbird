3fd234c5732407851e519e46c945b3497090928b
Patrick Pedersen <patrick.pedersen@nav.no>
2025-03-18 15:04:48 +0100

    feat: plan command gives a overview of changes

--- snowbird/main.py
+++ snowbird/main.py
@@ -1,4 +1,5 @@
 import json
+import re
 from pathlib import Path
 
 import click
@@ -52,10 +53,96 @@ def plan(config, silent, state, stateless):
     execution_plan = _setup_execution_plan(
         config=config, silent=silent, state=state, stateless=stateless
     )
+    create_databases = [s.split()[5] for s in execution_plan if "create database" in s]
+    alter_databases = [s.split()[2] for s in execution_plan if "alter database" in s]
+    modify_databases = [d for d in alter_databases if d not in create_databases]
+
+    create_schemas = [s.split()[5] for s in execution_plan if "create schema" in s]
+    alter_schemas = [s for s in execution_plan if "alter schema" in s]
+    modify_schemas = [
+        s.split()[2] for s in alter_schemas if s.split()[2] not in create_schemas
+    ]
+
+    create_roles = [s.split()[5] for s in execution_plan if "create role" in s]
+    alter_roles = [s.split()[2] for s in execution_plan if "alter role" in s]
+    modify_roles = [r for r in alter_roles if r not in create_roles]
+
+    create_users = [s.split()[5] for s in execution_plan if "create user" in s]
+    alter_users = [s.split()[2] for s in execution_plan if "alter user" in s]
+    modify_users = [u for u in alter_users if u not in create_users]
+
+    create_warehouses = [
+        s.split()[5] for s in execution_plan if "create warehouse" in s
+    ]
+    alter_warehouses = [s.split()[2] for s in execution_plan if "alter warehouse" in s]
+    print(alter_warehouses)
+    modify_warehouses = [w for w in alter_warehouses if w not in create_warehouses]
+
+    alter_warehouses = [s for s in execution_plan if "alter warehouse" in s]
+
+    grant_selects = [s for s in execution_plan if "grant select on" in s]
+    grant_create = [s for s in execution_plan if "grant create table" in s]
+    grant_roles = [s for s in execution_plan if "grant role" in s and "to role" in s]
+    grant_users = [s for s in execution_plan if "grant role" in s and "to user" in s]
+
     if silent == False:
-        click.echo("Execution plan")
-    for statement in execution_plan:
-        click.echo(f"{statement};")
+        click.echo("\nCreate or modify:")
+        click.echo("-----------------")
+
+        total_create_objects = len(
+            create_databases + create_schemas + create_roles + create_users
+        )
+        total_modify_objects = len(
+            modify_databases + modify_schemas + modify_roles + modify_users
+        )
+        click.echo(
+            "Total:".ljust(20)
+            + f"{str(total_create_objects).rjust(2)} create, {str(total_modify_objects).rjust(2)} modify\n"
+        )
+        click.echo(
+            "Databases:".ljust(20)
+            + f"{str(len(create_databases)).rjust(2)} create, {str(len(modify_databases)).rjust(2)} modify"
+        )
+        click.echo(
+            "Schemas:".ljust(20)
+            + f"{str(len(create_schemas)).rjust(2)} create, {str(len(modify_schemas)).rjust(2)} modify"
+        )
+        click.echo(
+            "Roles:".ljust(20)
+            + f"{str(len(create_roles)).rjust(2)} create, {str(len(modify_roles)).rjust(2)} modify"
+        )
+        click.echo(
+            "Users:".ljust(20)
+            + f"{str(len(create_users)).rjust(2)} create, {str(len(modify_users)).rjust(2)} modify"
+        )
+        click.echo(
+            "Warehouses:".ljust(20)
+            + f"{str(len(create_warehouses)).rjust(2)} create, {str(len(modify_warehouses)).rjust(2)} modify\n"
+        )
+
+        click.echo(f"Grant:")
+        click.echo("------")
+        click.echo(
+            "Total:".ljust(20)
+            + f"{str(len(grant_selects) + len(grant_create) + len(grant_roles) + len(grant_users)).rjust(2)} apply,   0 revoke\n"
+        )
+        click.echo(
+            "Read on schema:".ljust(20)
+            + f"{str(len(grant_selects)).rjust(2)} apply,   0 revoke"
+        )
+        click.echo(
+            "Write on schema:".ljust(20)
+            + f"{str(len(grant_create)).rjust(2)} apply,   0 revoke"
+        )
+        click.echo(
+            "Role to role:".ljust(20)
+            + f"{str(len(grant_roles)).rjust(2)} apply,   0 revoke"
+        )
+        click.echo(
+            "Role to user:".ljust(20)
+            + f"{str(len(grant_users)).rjust(2)} apply,   0 revoke"
+        )
+        click.echo("")
 
 
 @cli.command()
